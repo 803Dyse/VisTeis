477a4ac18628e817696f72fc79c5d9d6c7c5e1c2
803Dyse <alexdyse803@gmail.com>
2023-03-07 18:47:34 +0100

    Clase cell totalmente testeada, implementada y comentada.

--- src/visteis/Cell.java
+++ src/visteis/Cell.java
@@ -5,25 +5,42 @@
 package visteis;
 
 /**
+ * Esta clase genera las celdas de la interfaz gráfica, con esta clase
+ * conseguiremos implementar metodos que sacan la interfaz del juego (modo
+ * texto) por pantalla. Toda la malla del juego se crea a partir de aquí.
  *
  * @author Alejandro Martínez Domínguez, Bilo Alejandro Martins González y Raúl
  * Parada de la Fuente
  */
 public class Cell {
 
-    private boolean mined;
-    private int state;
-    private int raw;
-    private int column;
+    private boolean mined; //Este booleano indicará si la celda está tapada o no.
+    private int state; // Esta variable almacena el status del juego utilizando las constantes que declaramos abajo.
+    private int raw; // estas son las filas que utilizaremos en el bucle for para crear la malla del juego.
+    private int column; //y estas son las columnas, que se utiliza igual que las filas.
+
+    /**
+     * Este es la constante que define el estado de la celda como tapada
+     */
     public static final int STATE_HIDDEN = 1;
+
+    /**
+     * Este es la constante que define el estado de la celda como marcada
+     */
     public static final int STATE_CHECKED = 2;
+
+    /**
+     * Este es la constante que define el estado de la celda como destapada
+     */
     public static final int STATE_SHOWN = 3;
 
     /**
      * Siempre que crees un objeto de cell, aqui tienes la grade para crear las
-     * celulas, esto seria el atributo de game.
+     * celdas, esto seria el atributo de game.
      *
+     * @param mined
      * @param raw
+     * @param state
      * @param column
      */
     public Cell(boolean mined, int state, int raw, int column) {
@@ -33,66 +50,34 @@ public class Cell {
         this.column = column;
     }
 
-    /**
-     *
-     * @return
-     */
     public boolean isMined() {
         return mined;
     }
 
-    /**
-     *
-     * @param mined
-     */
     public void setMined(boolean mined) {
         this.mined = mined;
     }
 
-    /**
-     *
-     * @return
-     */
     public int getState() {
         return state;
     }
 
-    /**
-     *
-     * @param state
-     */
     public void setState(int state) {
         this.state = state;
     }
 
-    /**
-     *
-     * @return
-     */
     public int getRaw() {
         return raw;
     }
 
-    /**
-     *
-     * @param raw
-     */
     public void setRaw(int raw) {
         this.raw = raw;
     }
 
-    /**
-     *
-     * @return
-     */
     public int getColumn() {
         return column;
     }
 
-    /**
-     *
-     * @param column
-     */
     public void setColumn(int column) {
         this.column = column;
     }

ac6165acf7ee66de90ae9ccd2250ec9b28e29837
bmargon <bmargon@DAW1-33.dip.local>
2023-03-10 11:57:41 +0100

    [STABLE] Version final parte 1, punto de backup para empezar parte 2.

--- src/visteis/VisTeisMinas.java
+++ src/visteis/VisTeisMinas.java
@@ -19,6 +19,26 @@ public class VisTeisMinas {
      * @param args
      */
     public static void main(String[] args) {
+         /**
+         * Dejaré aqui comentado lo que pide el enunciado del proyecto porque lo
+         * que vereis a seguir es lo que entiendo que hay que hacer, de todos
+         * modos esta documentacion és crucial para que os situeis bien y si
+         * luego hay un error, que ya sabeis que aqui puede ser donde está. Aun
+         * que de momento entiendo que esto es lo que pide.
+         */
+        //Dejo el enunciado abajo de ESTE comentario.
+        /**
+         * No método "main" da clase "VisTeisMinas", se se recibe o parámetro
+         * "text" na liña de comandos se creará un obxecto da clase
+         * "VisteisMinasMenu" e se chamará ao método "startNewGame()" para que
+         * mostre a interface en modo texto, e se non se recibe, mostrarase a
+         * ventá "VisTeisMinasWindow".
+         */
+//        if (args.length == 1 && args[0].equals("text")) {
+//            (new VisTeisMinasMenu()).startNewGame();
+//        } else {
+//            (new VisTeisMinasMenu()).startNewGame();
+//        }
         VisTeisMinasMenu visTeisMinasMenu = new VisTeisMinasMenu();
         visTeisMinasMenu.startNewGame();
     }
